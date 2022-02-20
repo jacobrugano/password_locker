574ec820c627cf28283915d65f35c001e05e10a3
github_username <jacobrugano@gmail.com>
2022-02-20 23:32:38 +0300

    Added the code to enable a user delete his details

--- user_test.py
+++ user_test.py
@@ -36,12 +36,20 @@ class TestUser(unittest.TestCase):
              empty list. This helps us get accurate test results every time a new test case'''
 
              
-#   test_save_multiple_users to test if we can save multiple users in our users list.
+#   test_save_multiple_user' details to test if we can save multiple users in our users list.
     def test_save_multiple_users(self):
             self.new_user.save_user()  
-            test_user = User("Test","user","3456788") # code block for the extra new user
-            test_user.save_user() # saving the extra new user
-            self.assertEqual(len(User.user_list),2) # to check if the length of our users list is equal to the number of users saved.
+            test_user = User("Test","user","3456788") # code block for the extra new user details
+            test_user.save_user() # saving the extra new user details
+            self.assertEqual(len(User.user_list),2) # to check if the length of our user_list is equal to the number of users saved.
+
+#To test if a user can delete his user details from the user_list
+    def test_delete_user(self):
+            self.new_user.save_user()  # saving the new user
+            test_user = User("Test","user","0712345678")  # code block for the extra new user details
+            test_user.save_user()
+            self.new_user.delete_user()# Deleting a user object
+            self.assertEqual(len(User.user_list),1)# to check if the length of our user_list is equal to the number of users saved.
 
 if __name__ == '__main__':
     unittest.main()
