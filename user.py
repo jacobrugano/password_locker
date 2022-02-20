48b8574121c35ba7ad7b44da8e2833d26132f8b0
github_username <jacobrugano@gmail.com>
2022-02-20 22:25:53 +0300

    Added the user_test code and imported from the user.py

--- user.py
+++ user.py
@@ -1,15 +1,15 @@
-class User:
-    """
-    Class that generates new instances of contacts
-    """
-    def __init__(self,first_name,last_name,password):
-
-        ''' __init__ method to create new instances of a class User and to allow us to 
-            pass in properties for the new object.   
-        Args:
-            first_name: User's first name.
-            last_name : User's last name.
-            pasword: User's password.
-        '''
+class User:   #Class that generates new instances of contacts
+    
     user_list = [] #user_list is a Class variables(variables that belong to the entire class and can be  accessed by all instances of the class. This user_list var will be used to store our created user objects
-       
+
+    def __init__(self,first_name,last_name,password):
+                            ''' __init__ method to create new instances of a class User and to allow us to 
+                                pass in properties for the new object.   
+                            Args:
+                                first_name: User's first name.
+                                last_name : User's last name.
+                                pasword: User's password.
+                            '''  
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from user import User # Importing the user class
+
+class TestUser(unittest.TestCase):
+    '''
+    We create a subclass class called TestUser, that inherits from unittest.TestCase and defines
+    test cases for the contact class behaviours.
+    '''
+
+if __name__ == '__main__':
+    unittest.main()
