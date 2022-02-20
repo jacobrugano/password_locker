34ed460711fb9823ce24eabb6bf50ff76cca6805
github_username <jacobrugano@gmail.com>
2022-02-20 22:55:15 +0300

    Added the code block to add all users to the user_list using .append() method

--- user.py
+++ user.py
@@ -1,9 +1,13 @@
 class User:   #Class that generates new instances of contacts
     
     user_list = [] #user_list is a Class variables(variables that belong to the entire class and can be  accessed by all instances of the class. This user_list var will be used to store our created user objects
-
     def __init__(self,first_name,last_name,password):
                             #  __init__ method to create new instances of a class User and to allow us to pass in properties for the new object.
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
+
+
+# we create a save_user() method and called it on User object to save users into the user_list using append() method.'''  
+    def save_user(self):  
+        User.user_list.append(self) 
