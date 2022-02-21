e15e880e9fe41474ca862b062141878e09a95535
github_username <jacobrugano@gmail.com>
2022-02-21 19:35:03 +0300

    Created the credentials class and created the credentials class

---
+++ credentials.py
@@ -0,0 +1,3 @@
+class Credentials:   #Class that generates new instances of credentials
+    
+    credential_list = [] #credential_list is a Class variables(variables that belong to the entire class and can be  accessed by all instances of the class. This user_list var will be used to store our created user objects
