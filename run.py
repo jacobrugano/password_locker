9f267c328ed3442db11f33fb0a767389a07bde2e
github_username <jacobrugano@gmail.com>
2022-02-25 14:33:34 +0300

    Created the function in the run.py to save the credentials entered

--- run.py
+++ run.py
@@ -8,3 +8,10 @@ def create_credential(user_name,pass_word,phone,email):
     '''
     new_credential = Credential(user_name,pass_word,phone,email)
     return new_credential
+
+#We create a save_credential function that takes in a credential/contact object and then calls the save_contact method to save the credentials.
+def save_credential(credential):
+    '''
+    Function to save credentials
+    '''
+    credential.save_credential()
